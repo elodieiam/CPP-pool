7df252b79814b6135e1e0065ce49e79dd77b355e
elodie <elodie@LAPTOP-FI669H0F>
2024-02-24 18:04:55 +0100

    cpp00 ex00 done in c++ way

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -5,37 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/22 14:29:23 by elrichar          #+#    #+#             */
-/*   Updated: 2024/02/22 16:19:53 by elrichar         ###   ########.fr       */
+/*   Created: 2024/02/24 17:26:10 by elrichar          #+#    #+#             */
+/*   Updated: 2024/02/24 18:03:51 by elrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	int 	i(1);
-	int		j(0);
-	char	c('1');
+	std::string	str("");
 	
 	if (ac == 1)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 0);
+	for (int i = 1; av[i]; i++)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		return (0);
+		str = av[i];
+		for (int j = 0; av[i][j]; j++)
+			str[j] = std::toupper(str[j]);
+		std::cout << str;
+		if (av[i])
+			std::cout << " ";
 	}
-	while (av[i])
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			c = std::toupper(av[i][j]);
-			std::cout << c;
-			j++;
-		}
-		std::cout << " ";
-		i++;
-	}
-	std::cout << "\n";
-	return (0);
+	return (std::cout << "\n", 0);
 }
