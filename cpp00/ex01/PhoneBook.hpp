798b65819ace226b7c9fad6687651d1e0781509f
el0die21 <elodie15@gmail.com>
2024-03-06 13:34:48 +0100

    small fix

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 15:54:28 by elrichar          #+#    #+#             */
-/*   Updated: 2024/03/04 12:02:49 by elrichar         ###   ########.fr       */
+/*   Updated: 2024/03/06 13:34:30 by elrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class	PhoneBook
 {
 	public:
-	PhoneBook();
+	PhoneBook(void);
 	~PhoneBook(void);
 	void	add_contact(int	index);
 	void	display_contact(int index, int nb_contacts);
