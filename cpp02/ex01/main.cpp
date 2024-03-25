4c800c770a7140589f7730bd30e7706d3cec2742
el0die21 <elodie15@gmail.com>
2024-03-25 19:07:26 +0100

    cpp2 ex01 + beginning of ex2

---
+++ cpp02/ex01/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 16:19:12 by elrichar          #+#    #+#             */
+/*   Updated: 2024/03/25 18:10:00 by elrichar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed const b( 10 );
+Fixed const c( 42.42f );
+Fixed const d( b );
+a = Fixed( 1234.4321f );
+Fixed const e( 0.0f );
+Fixed const f( 1.0465765576557f );
+Fixed const g( 104657.65576557f );
+
+
+std::cout << "a is " << a << std::endl;
+std::cout << "b is " << b << std::endl;
+std::cout << "c is " << c << std::endl;
+std::cout << "d is " << d << std::endl;
+std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+std::cout << "e is " << e.toInt() << " as integer" << std::endl;
+std::cout << "f is " << f.toInt() << " as integer" << std::endl;
+std::cout << "g is " << g.toInt() << " as integer" << std::endl;
+
+
+return 0;
+}
--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/21 18:00:50 by elrichar          #+#    #+#             */
-/*   Updated: 2024/03/25 16:46:27 by elrichar         ###   ########.fr       */
+/*   Created: 2024/03/25 18:25:58 by elrichar          #+#    #+#             */
+/*   Updated: 2024/03/25 18:45:31 by elrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ class Fixed
 	Fixed(const int fixed_point);
 	Fixed(const float fixed_point);
 	Fixed (const Fixed &other);
-	Fixed &operator = (const Fixed &other);
 	~Fixed(void);
 	
 	int	getRawBits(void) const;
@@ -32,11 +31,34 @@ class Fixed
 	float toFloat( void ) const;
 	int toInt( void ) const;
 
+	Fixed &operator = (const Fixed &other);
+	
+	
+
+	bool			operator<(const Fixed &a, const Fixed &b);
+	bool			operator>(const Fixed &a, const Fixed &b);
+	bool			operator>=(const Fixed &a, const Fixed &b);
+	bool			operator<=(const Fixed &a, const Fixed &b);
+	bool			operator==(const Fixed &a, const Fixed &b);
+	bool			operator!=(const Fixed &a, const Fixed &b);
+
+	Fixed			operator+(const Fixed &a, const Fixed &b);
+	Fixed			operator-(const Fixed &a, const Fixed &b);
+	Fixed			operator*(const Fixed &a, const Fixed &b);
+	Fixed			operator/(const Fixed &a, const Fixed &b);
+
+	static Fixed& min(Fixed& fixed1, Fixed& fixed2);
+	static const Fixed& min(const Fixed& fixed1, const Fixed& fixed2);
+	static Fixed& max(Fixed& fixed1, Fixed& fixed2);
+	static const Fixed& max(const Fixed& fixed1, const Fixed& fixed2);
+	
 	private:
 	int	_fixedValue;
 	static const int _fractBits = 8;	
 };
 
 std::ostream	&operator<<(std::ostream &os, const Fixed &fixed);
+
+
 
 #endif
